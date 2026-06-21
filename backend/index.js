3cacc026b38a00d964b273796201c3f26b233b22
Yash <yash@Yashs-MacBook-Air.local>
2026-06-21 13:13:24 +0530

    backend for mini-url-shortner

---
+++ backend/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const cors = require('cors');
+const { nanoid } = require('nanoid');
+
+app.use(express.json())
+app.use(cors());
+
+dotenv.config();
+
+let urlDatabase = []
+
+app.post('/api/shorten' , (req, res)=>{
+    // extract 'originalUrl' from req.body
+    const { originalUrl } = req.body;
+    console.log("The user wants to shorten", originalUrl);
+
+    if (!originalUrl){
+        return res.status(400).json({
+            msg: "Original URL not found"
+        })
+    }
+
+    const shortCode = nanoid(6)
+    console.log(shortCode)
+
+    const newUrl = {
+        id : nanoid(),
+        originalUrl,
+        shortCode,
+        clicks: 0,
+        createdAt: new Date(),
+    }
+
+    urlDatabase.push(newUrl);
+    return res.status(201).json({
+        msg: "Short URL created successfully",
+        data: newUrl
+    })
+})
+
+app.get('/:shortCode', (req,res)=>{
+    const { shortCode } = req.params
+    //finding url in arr
+    const urlEntry = urlDatabase.find((entry) => entry.shortCode === shortCode)
+
+    if(!urlEntry){
+        return res.status(404).json({
+            msg: "URL entry not found"
+        })
+    }
+    urlEntry.clicks++
+    res.redirect(urlEntry.originalUrl)
+})
+
+app.get('/api/analytics/:shortCode', (req,res)=>{
+    const { shortCode } = req.params
+    const urlEntry = urlDatabase.find((entry) => entry.shortCode === shortCode)
+
+    if(!urlEntry){
+        return res.status(404).json({
+            msg: "URL entry not found"
+        })
+    }
+
+    return res.json({
+        clicks: urlEntry.clicks
+    })
+})
+
+
+app.get('/', (req, res) =>{
+    res.send('Hello World!');
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+})
